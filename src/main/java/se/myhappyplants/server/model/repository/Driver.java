cea19f79187e156ba80879263ef4889b36116f11
fridajacobsson <frida_jacobsson@live.se>
2021-04-14 10:21:22 +0200

    Changes from local db.

--- src/main/java/se/myhappyplants/server/model/repository/Driver.java
+++ src/main/java/se/myhappyplants/server/model/repository/Driver.java
@@ -20,7 +20,7 @@ public class Driver {
     String dbServerPort = PasswordsAndKeys.dbServerPort;
     String dbUser = PasswordsAndKeys.dbUsername;
     String dbPassword = PasswordsAndKeys.dbPassword;
-    String dbLinnLocal = PasswordsAndKeys.dbLinnLocal;
+    //String dbLinnLocal = PasswordsAndKeys.dbLinnLocal;
 
     //If the DB host connects from their own IP then change connection to localhost
     if (InetAddress.getLocalHost().getHostName().equals(PasswordsAndKeys.dbHostName)) {
@@ -39,3 +39,5 @@ public class Driver {
     return conn;
   }
 }
+
+
