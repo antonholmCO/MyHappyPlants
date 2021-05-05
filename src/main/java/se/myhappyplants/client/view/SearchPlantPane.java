6c51a69ec99a0d72dda62ff9561d7a7566c07b39
Raggmunk2 <76004088+Raggmunk2@users.noreply.github.com>
2021-05-05 16:25:59 +0200

    changed after the new stylesheet

--- src/main/java/se/myhappyplants/client/view/SearchPlantPane.java
+++ src/main/java/se/myhappyplants/client/view/SearchPlantPane.java
@@ -33,6 +33,7 @@ public class SearchPlantPane extends Pane {
     private Label lblFamilyName;
     private Label lblLightText;
     private Label lblWaterText;
+    private Label lblGenusText;
     private boolean gotInfoOnPlant;
 
     private ObservableList<String> getAllPlantInfo;
@@ -94,7 +95,7 @@ public class SearchPlantPane extends Pane {
         addButton.setOnAction(action -> plantsTabController.addPlantToCurrentUserLibrary(dbPlant));
 
         listView = new ListView();
-        listView.setLayoutX(90.0); //this.getWidth()
+        listView.setLayoutX(110.0); //this.getWidth()
         listView.setLayoutY(this.getHeight() + 56.0);
         listView.setPrefWidth(651.0); //751.0
         listView.setPrefHeight(150.0);
@@ -102,23 +103,31 @@ public class SearchPlantPane extends Pane {
         lblFamilyName = new Label();
         lblFamilyName.setText("Family name: ");
         lblFamilyName.setLayoutX(5.0);
-        lblFamilyName.setLayoutY(60.0);
+        lblFamilyName.setLayoutY(67.0);
         lblFamilyName.setPrefHeight(15.0);
         lblFamilyName.setPrefWidth(100.0);
 
         lblLightText = new Label();
         lblLightText.setText("Light: ");
         lblLightText.setLayoutX(5.0);
-        lblLightText.setLayoutY(85.0);
+        lblLightText.setLayoutY(95.0);
         lblLightText.setPrefHeight(15.0);
         lblLightText.setPrefWidth(100.0);
 
         lblWaterText = new Label();
         lblWaterText.setText("Water: ");
         lblWaterText.setLayoutX(5.0);
-        lblWaterText.setLayoutY(105.0);
+        lblWaterText.setLayoutY(119.0);
         lblWaterText.setPrefHeight(15.0);
         lblWaterText.setPrefWidth(100.0);
+
+        lblGenusText = new Label();
+        lblGenusText.setText("Genus: ");
+        lblGenusText.setLayoutX(5.0);
+        lblGenusText.setLayoutY(144.0);
+        lblGenusText.setPrefHeight(15.0);
+        lblGenusText.setPrefWidth(100.0);
+
 
         this.prefHeight(56.0);
         this.prefWidth(761.0);
@@ -147,7 +156,7 @@ public class SearchPlantPane extends Pane {
         );
         timeline.setCycleCount(4);
         timeline.play();
-        timeline.setOnFinished(action -> this.getChildren().addAll(listView,lblFamilyName,lblLightText,lblWaterText));
+        timeline.setOnFinished(action -> this.getChildren().addAll(listView,lblFamilyName,lblLightText,lblWaterText,lblGenusText));
         extended = true;
         gotInfoOnPlant = true;
 
