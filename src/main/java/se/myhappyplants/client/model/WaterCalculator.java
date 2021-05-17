df1e76351d9f6ed93957ccb415301d7dfcba05c3
Susanne Vikström <Susanny@live.se>
2021-05-17 14:51:17 +0200

    moved logic from getWaterFrequency in PlantRepository to WaterCalculator

--- src/main/java/se/myhappyplants/client/model/WaterCalculator.java
+++ src/main/java/se/myhappyplants/client/model/WaterCalculator.java
@@ -6,7 +6,7 @@ package se.myhappyplants.client.model;
  */
 public class WaterCalculator {
 
-    public String calculateWater(String minWater) {
+    public String calculateWaterLevelToString(String minWater) {
         String waterText;
 
         int waterFrequencyInt = Integer.parseInt(minWater);
@@ -28,7 +28,29 @@ public class WaterCalculator {
         else {
             waterText = "There's no information about the water level";
         }
-
         return waterText;
+    }
+
+    public long calculateWaterFrequencyForWatering(String waterFrequency){
+        long waterFrequencyMilli = 0;
+
+        long week = 604000000l;
+        int waterFrequencyInt = Integer.parseInt(waterFrequency);
+        if (waterFrequencyInt <= 200) {
+            waterFrequencyMilli = week * 4;
+        }
+        else if (waterFrequencyInt > 200 && waterFrequencyInt <= 400) {
+            waterFrequencyMilli = week * 3;
+        }
+        else if (waterFrequencyInt > 400 && waterFrequencyInt <= 600) {
+            waterFrequencyMilli = week * 2;
+        }
+        else if (waterFrequencyInt > 600 && waterFrequencyInt <= 800) {
+            waterFrequencyMilli = week * 1;
+        }
+        else if (waterFrequencyInt > 800) {
+            waterFrequencyMilli = week / 2;
+        }
+        return waterFrequencyMilli;
     }
 }
